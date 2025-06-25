7223b800b6ef7ba139b2a51cf59e19897d6d4216
crea8ivedev <crea8ivedev@gmail.com>
2025-06-25 19:23:45 +0530

    refactor: homepage

--- assets/custom.js
+++ assets/custom.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
-  new Swiper('#featured-gallery', {
+document.addEventListener("DOMContentLoaded", function () {
+  new Swiper("#featured-gallery", {
     loop: true,
     pagination: {
-      el: '.swiper-pagination',
+      el: ".swiper-pagination",
       clickable: true,
     },
     slidesPerView: 1,
@@ -10,25 +10,20 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-
-
 function scrollDownByBanner() {
-    const banner = document.querySelector('.custom-slider-wrapper');
-    if (banner) {
-      window.scrollBy({
-        top: banner.offsetHeight,
-        left: 0,
-        behavior: 'smooth'
-      });
-    }
-  } 
-
-
-  document.addEventListener("DOMContentLoaded", function () {
-    const backToTopButton = document.querySelector(".back-to-top-button");
-    backToTopButton.addEventListener("click", function () {
-      window.scrollTo({ top: 0, behavior: "smooth" });
+  const banner = document.querySelector(".custom-slider-wrapper");
+  if (banner) {
+    window.scrollBy({
+      top: banner.offsetHeight,
+      left: 0,
+      behavior: "smooth",
     });
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const backToTopButton = document.querySelector(".back-to-top-button");
+  backToTopButton.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
   });
-
-
+});
